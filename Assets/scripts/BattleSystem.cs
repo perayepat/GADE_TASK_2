053c92260be31032b86ea1abed2a5edef59c177f
perayepat <56199797+perayepat@users.noreply.github.com>
2021-06-17 15:28:43 +0200

    fresh start

--- Assets/scripts/BattleSystem.cs
+++ Assets/scripts/BattleSystem.cs
@@ -1,20 +1,37 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 namespace m.m.TurnBasedGame
 {
     public class BattleSystem : MonoBehaviour
     {
         [SerializeField] private HUD hud;
+        [SerializeField] private UnitHUD playerHUD, enemyHUD;
         [SerializeField] private GameObject playerPrefab;
         [SerializeField] private GameObject enemyPrefab;
         [SerializeField] private Transform playerPosition;
         [SerializeField] private Transform enemyPosition;
-
+        public GameObject itemsPanel;
+        private int onAndOffTing = 1;
         private Unit _playerUnit;
         private Unit _enemyUnit;
         private State _currentState;
         private Setdifficulty setdifficulty;
+
+        public UnitHUD PlayerHUD
+        {
+            get { return playerHUD; }
+            set { playerHUD = value; }
+        }
+
+        public UnitHUD EnemyHUD
+        {
+            get { return enemyHUD; }
+            set { enemyHUD = value; }
+        }
+
+
 
         public Unit PlayerUnit => _playerUnit;
         public Unit EnemyUnit => _enemyUnit;
@@ -22,9 +39,10 @@ namespace m.m.TurnBasedGame
 
         public Setdifficulty Setdifficulty => setdifficulty;
 
+        
         public void SetState(State state)
         {
-            
+            //making running each states start easier from other classes 
             _currentState = state;
             StartCoroutine(_currentState.Start());
         }
@@ -55,18 +73,31 @@ namespace m.m.TurnBasedGame
             StartCoroutine(_currentState.Blocking());
         }
 
-        public void onUseItemButton()
+        public void OnSapButton()
         {
-            Debug.Log("Items");
-            //open the menu and handle the buttons in the menu
+            StartCoroutine(_currentState.Sapping());
+
+        }
+
+        public void OnStackButton()
+        {
+            StartCoroutine(_currentState.Stacking());
+        }
+
+        public void OnRestButton()
+        {
+            StartCoroutine(_currentState.Resting());
         }
 
         private void Start()
         {
-            
             SetupBattle();
         }
 
+        public void OnEscapeButton()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
         private void SetupBattle()
         {
             var playerGameObject = Instantiate(playerPrefab, playerPosition);
@@ -78,6 +109,17 @@ namespace m.m.TurnBasedGame
             hud.Initialize(_playerUnit, _enemyUnit);
             
             SetState(new BeginState(this));
+        }
+
+        public void OnItemsPanelEnable()
+        {
+            
+            onAndOffTing ++;
+            itemsPanel.active = false;
+            if (onAndOffTing % 2 == 0)
+            {
+                itemsPanel.active = true;
+            }
         }
     }
 
