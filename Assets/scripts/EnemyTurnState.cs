543071d1f3c9c434427fb220c5d46b8904cca90b
perayepat <56199797+perayepat@users.noreply.github.com>
2021-06-17 15:29:03 +0200

    fs

--- Assets/scripts/EnemyTurnState.cs
+++ Assets/scripts/EnemyTurnState.cs
@@ -1,60 +1,100 @@
 using System.Collections;
 using UnityEngine;
-//public enum enemy
-//{
-//HARD ,
-//EASY,
-//MEDUIM
-//}
+public enum enemy
+{
+    HARD,
+    EASY,
+    MEDUIM,
+    NONE
+}
 //make these difficulties thier own states and depending on what difficulty the player chose in battle system 
 //that state will be called 
 
 namespace m.m.TurnBasedGame
 {
+    
     public class EnemyTurnState : State
     {
+        public static EnemyTurnState instance = null;
+        private void Awake()
+        {
+            if (instance == null)
+                instance = this;
+            else if (instance != this)
+                Destroy(gameObject);
+            
+        }
+        bool easyOn;
+
+        
+        public enemy _enemy;
         public EnemyTurnState(BattleSystem system) : base(system)
         {
         }
 
         public override IEnumerator Start()
         {
-            //use a switch to run the different values 
-            #region This all goes in the easy class which will be ran if the easy level is chosen 
-            battleSystem.HUD.SetDialogText($"{battleSystem.EnemyUnit.Name} attacks!");
-
-
             
-            //handelling blocking 
-            var isDead = battleSystem.PlayerUnit.TakeDamage(battleSystem.EnemyUnit.Damage);
-            if (battleSystem.PlayerUnit.blocking == true)
+          
+            switch (_enemy)
             {
-                isDead = battleSystem.PlayerUnit.isBlocking(battleSystem.EnemyUnit.Damage, battleSystem.PlayerUnit.Block);
-                battleSystem.HUD.SetDialogText($"{battleSystem.PlayerUnit.Name}Blocked");
-                battleSystem.PlayerUnit.blocking = false;
+                case enemy.HARD:
+                    battleSystem.SetState(new HardEnemy(battleSystem));
+                    break;
+                case enemy.EASY:
+                    battleSystem.SetState(new EasyEnemy(battleSystem));
+                    break;
+                case enemy.MEDUIM:
+                    battleSystem.SetState(new MeduimEnemy(battleSystem));
+                    break;
+                default:
+                    break;
             }
-            else
-            {
-                isDead = battleSystem.PlayerUnit.TakeDamage(battleSystem.EnemyUnit.Damage);
-                battleSystem.HUD.SetDialogText($"{battleSystem.EnemyUnit.Name} attacks!");
-            }
+
             yield return new WaitForSeconds(1f);
 
-            if (isDead)
-            {
-                battleSystem.SetState(new LostState(battleSystem));
-            }
-            else
-            {
-                battleSystem.SetState(new PlayerTurnState(battleSystem));
-            }
-            #endregion
+           
+
         }
+        public void OnSetEnemyLevelEasy()
+        {
+            _enemy = enemy.EASY;
+        }
+
+        public void ONSetEnemyLevelMeduim()
+        {
+            _enemy = enemy.MEDUIM;
+        }
+        public void ONSetEnemyLevelHard()
+        {
+            _enemy = enemy.HARD;
+        }
+
+
 
         public override IEnumerator Pause()
         {
             battleSystem.SetState(new PauseState(battleSystem, this));
             yield break;
         }
+
+        #region Buttons 
+        public void OnEasyButton()
+        {
+            _enemy = enemy.EASY; 
+        }
+        public void OnMeduimButton()
+        {
+            _enemy = enemy.MEDUIM;
+        }
+        public void OnHardButton()
+        {
+            _enemy = enemy.HARD;
+        }
+        #endregion
     }
+
+    
+
 }
+
